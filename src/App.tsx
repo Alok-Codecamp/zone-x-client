8a9667275863c609aaa7f23e99c94b0ff4ef9e28
Alok-Codecamp <alok61.bd@gmail.com>
2025-06-04 05:02:40 +0600

    project initialization complte

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Input } from "./components/ui/input"
+
+
+function App() {
+  return (
+    <div className='bg-green-500 h-screen'>
+      <Input placeholder="email" />
+    </div>
+  )
+}
+
+export default App
